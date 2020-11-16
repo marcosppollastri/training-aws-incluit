5d74fe46ff8b84a46bf66a57a6610a198fced615
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2020-11-16 17:44:31 -0300

    ejercicio 1

--- publish-message/index.js
+++ publish-message/index.js
@@ -5,13 +5,25 @@ const sqs = new AWS.SQS();
 
 exports.handler = async (event) => {
     try {
+        let regex = /^\w+$/;
+        let name = JSON.parse(event.body).name || null;
+        if(name === undefined) {name = "";}
+        if(name && regex.test(name)){
+            await sqs.sendMessage({
+                MessageBody: JSON.stringify({
+                    name: event.name || 'Prueba'
+                }),
+                QueueUrl: 'https://sqs.us-east-1.amazonaws.com/437051718508/Pepe-queue'
+            }).promise();
+        } else {
+            console.log('El nombre solo puede ser alfanumerico');
+            return {
+                statusCode: 500,
+                body: JSON.stringify({"message": "Nombre invalido"})
+            }
+        }
         // All messages on SQS *MUST* be a String
-        await sqs.sendMessage({
-            MessageBody: JSON.stringify({
-                name: event.name || 'Prueba'
-            }),
-            QueueUrl: 'https://sqs.us-east-1.amazonaws.com/437051718508/Pepe-queue'
-        }).promise();
+
     }
     catch (err) {
         console.log(err);
